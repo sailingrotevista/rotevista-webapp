3cf6e6a183730ab10bf818ee231f34f283bf95f0
Alessio Tosi <alex@taco.it>
2026-03-27 19:27:03 +0100

    Fix for SignalK: change outDir to public

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Fondamentale per SignalK
+  build: {
+    outDir: 'public', // Cambiamo dist in public per SignalK
+    emptyOutDir: true,
+  }
 })
