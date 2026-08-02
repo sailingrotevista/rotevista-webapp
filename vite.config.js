9d95a40abdf6984e8ddaa33f2dbc0d07ec045973
Alessio Tosi <alex@taco.it>
2026-08-02 19:02:01 +0300

    Bug Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  server: {
-    watch: {
-      // Ignora la cartella public e node_modules per evitare riavvii infiniti
-      ignored: ['**/public/**', '**/node_modules/**'],
-    },
-  },
   build: {
-    outDir: 'public',
+    outDir: 'dist', // Impostato l'output standard su 'dist' per preservare la cartella 'public'
     emptyOutDir: true,
   }
 })
